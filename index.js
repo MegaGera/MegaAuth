d34bdbdce9eab80bad22ddba83dcf4ba0cfcc3f2
MegaGera <gonzalo.berne@gmail.com>
2025-09-15 00:10:25 +0100

    Redirect google oauth

--- index.js
+++ index.js
@@ -67,7 +67,9 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: process.env.NODE_ENV === 'production'
+    ? 'https://megagoal.megagera.com/auth/google/callback'
+    : '/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const { id, emails } = profile;
